c8df4ee5e1d97ea8a08aadb72d956fe115d23650
Dan Brett <boristhellama99@gmail.com>
2018-01-15 16:45:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function viewCart() {
 function total() {
   var total = 0;
   for (var i = 0; i < cart.length; i++) {
-    total = total + Objects.values(cart);
+    total = total + Object.values(cart);
   }
   return total;
 }
